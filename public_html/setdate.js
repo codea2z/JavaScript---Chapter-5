14b5e53f2718f09dd0b44a265ff36450b5f41524
codea2z <paul_savage@btinternet.com>
2014-03-27 17:06:30 +0000

    Extracting time components

---
+++ public_html/setdate.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function init()
+{
+    var panel = document.getElementById("panel");
+    var hol = new Date(2014, 6, 4);
+    panel.innerHTML += "Object: " + hol;
+    hol.setFullYear(2013);
+    hol.setMonth(11);
+    hol.setDate(25);
+    hol.setHours(12);
+    hol.setMinutes(0);
+    hol.setSeconds(0);
+    hol.setMilliseconds(0);
+    
+    panel.innerHTML += "<br>String: " + hol.toString();
+    panel.innerHTML += "<br>UTC: " + hol.toUTCString();
+    panel.innerHTML += "<br>Locale: " + hol.toLocaleString();
+    panel.innerHTML += "<br>Date: " + hol.toDateString();
+    panel.innerHTML += "<br>Time: " + hol.toTimeString();
+    
+}
+
+document.addEventListener("DOMContentLoaded", init, false);
+
